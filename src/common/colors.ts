33e1712b497b9ca223f00ab4b234db1e72ce9001
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2022-10-22 00:08:33 -0300

    feat(common): colors config

---
+++ src/common/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  black: '#000000',
+  gray: '#4E4E4E',
+  gray_light: '#C4C4C4' ,
+  white: '#FFFFFF',
+
+  font: '#000000',
+  font_light: '#656565',
+
+  primary: '#25A0B0',
+  primary_light: '#E1F6F4',
+
+  secondary: '#F5C8C6',
+
+  cream: '#EEF2E2',
+}
