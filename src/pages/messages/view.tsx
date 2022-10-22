269eef2f1bb647abc1ae5cf2381a90be3f0383f8
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2022-10-22 00:51:54 -0300

    feat: add navigation

---
+++ src/pages/messages/view.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const MessagesView = () => {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+export default MessagesView;
---
+++ src/pages/welcome/view.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const WelcomeView = () => {
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+export default WelcomeView;
